561b1572d7fee80a7eb49f8fad3d40c699b4b97a
pavithrathayal294917 <158862083+pavithrathayal294917@users.noreply.github.com>
2024-02-06 14:03:08 +0530

    Create FindNumber.java

---
+++ 03_02_2024_Assignments/FindNumber.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=45;
+		if (num>0) {
+			System.out.println("The Number " + num + " is Positive");
+		}
+		else {
+			System.out.println("The Number " + num + " is Negative");
+		}
+
+	}
+
+}
