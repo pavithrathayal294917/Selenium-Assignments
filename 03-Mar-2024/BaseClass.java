503f7889159436886afd0e1f5a19e1fc84946b9e
pavithrathayal294917 <158862083+pavithrathayal294917@users.noreply.github.com>
2024-03-08 05:04:38 +0530

    Create BaseClass.java

---
+++ 03-Mar-2024/BaseClass.java
@@ -0,0 +1,45 @@
+package week5.day2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+public class BaseClass {
+	
+	public RemoteWebDriver driver ;
+	@Parameters({"browser","url","username","password"})
+	@BeforeMethod
+	public void preCondition(String browser,String url,String username,String password) {
+		if(browser.equalsIgnoreCase("chrome")){
+	    driver  = new ChromeDriver();
+		}
+		else if (browser.equalsIgnoreCase("Edge")) {
+	    driver  = new EdgeDriver();
+		}
+		else
+		{
+			driver  = new SafariDriver();
+		}
+		driver.manage().window().maximize();
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+	}
+	
+	
+	@AfterMethod
+	public void postCondition(){
+		driver.close();
+		
+	}
+}
